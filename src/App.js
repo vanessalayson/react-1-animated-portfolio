90865d275ed1d9da2eeb239910b0a66d42cc1a5b
vanessalayson <vanessalayson25@gmail.com>
2024-02-21 19:41:07 +0800

    Second commit. Project 100% complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -7,6 +12,11 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
